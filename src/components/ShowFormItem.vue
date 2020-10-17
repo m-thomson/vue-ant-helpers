4e45d0243016537a75b4b5182e86f6f7fafa6757
Mark Thomson <hello@markthomson.ca>
2020-10-16 18:14:17 -0700

    css cleanup

--- src/components/ShowFormItem.vue
+++ src/components/ShowFormItem.vue
@@ -102,63 +102,89 @@ export default Vue.extend({
 </script>
 <!--suppress CssFloatPxLength -->
 <style lang="scss">
+// --------------------------------------------------
+// Variables
+// --------------------------------------------------
 :root {
   --form-item-label-height:   39.999px; /* antd value */
   --form-item-element-height: 40px; /* antd sets this with line-height */
   --form-item-help-height:    22px; /* antd sets this with font-size:14px, line-height:1.5, margin-top:-2px, margin-bottom:-1px */
   --form-item-total-height:   calc(var(--form-item-label-height) + var(--form-item-element-height) + var(--form-item-help-height));
+  --form-item-hover-z-index:  1;
+  --form-item-focus-z-index:  2;
+  --form-item-error-bg:       rgba(245, 34, 45, 0.2);
+  --form-item-border-radius:  4px;
   --form-gutter:              12px;
   --form-gutter-half:         calc( var(--form-row-gutter) / 2 );
   --form-gutter-half-neg:     calc( var(--form-row-gutter) / -2 );
 }
 
-.form-row {
-  margin-left:  -8px;
-  margin-right: -8px;
+// --------------------------------------------------
+// Emulating <a-row>
+// --------------------------------------------------
+div.form-row {
+  margin-left:  var(--form-gutter-half-neg);
+  margin-right: var(--form-gutter-half-neg);
   display:      flex;
   flex-flow:    row wrap;
 }
 
-/*noinspection CssUnusedSymbol*/
+// --------------------------------------------------
+// Emulating <a-col>
+// --------------------------------------------------
 div.form-col {
-  padding-left:  8px;
-  padding-right: 8px;
+  padding-left:  var(--form-gutter-half);
+  padding-right: var(--form-gutter-half);
   position:      relative;
   min-height:    1px;
   &.merge-left {
     padding-left: 0;
     margin-left:  -1px;
-    * { border-radius: 0 4px 4px 0; }
+    * { border-radius: 0 var(--form-item-border-radius) var(--form-item-border-radius) 0; }
   }
   &.merge-right {
     padding-right: 0;
-    * { border-radius: 4px 0 0 4px; }
+    * { border-radius: var(--form-item-border-radius) 0 0 var(--form-item-border-radius); }
   }
   &.merge-left.merge-right {
     * { border-radius: 0;}
   }
-  /* Hovered element should raise it's z-index so shared borders highlight correctly */
   &:hover {
-    z-index:1;
+    // Hovered element should raise it's z-index so
+    // shared borders highlight correctly
+    z-index: var(--form-item-hover-z-index);
   }
 }
 
-/* Focused elements should have highest z-index */
-input:focus,
-.ant-select-focused {
-  z-index:2;
+// --------------------------------------------------
+// Focused items have highest z-index
+// --------------------------------------------------
+.ant-form-item-control:focus-within {
+  z-index: var(--form-item-focus-z-index);
 }
 
-/* Ant forces error/warning colors on *all* applicable descendants of .has-error
-   and .has-warning. With this fix, only immediate descendants will be affected. */
-.ant-form-item-control:not(.has-error):not(.has-warning) {
+// --------------------------------------------------
+// Background for form items having errors
+// --------------------------------------------------
+.ant-form-item-control .has-error {
+  .ant-input,
+  .ant-input:hover,
+  .ant-select-selection {
+    background-color: var(--form-item-error-bg)
+  }
+}
 
+// --------------------------------------------------
+// Ant forces error/warning colors on *all* applicable
+// descendants of .has-error and .has-warning. With
+// this fix, only immediate descendants will be affected.
+// --------------------------------------------------
+.ant-form-item-control:not(.has-error):not(.has-warning) {
   // Help text
   .ant-form-explain,
   .ant-form-split {
     color:inherit;
   }
-
   // Icons inside form element.
   .ant-calendar-picker-icon::after,
   .ant-time-picker-icon::after,
@@ -166,13 +192,6 @@ input:focus,
   .ant-select-arrow,
   .ant-cascader-picker-arrow {
     color: inherit;
-  }
-
-  .ant-input,
-  .ant-input:hover,
-  .ant-input:focus,
-  .ant-select-selection {
-    //border-color: #d9d9d9;
   }
 }
 
