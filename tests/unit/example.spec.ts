e904026a7ae522da38e19d09919398a4afd973bc
Mark Thomson <hello@markthomson.ca>
2020-10-17 22:24:11 -0700

    add tests

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,29 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// noinspection ES6PreferShortImport
+import { _ant } from '../../src/antTools'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Ant tools', () => {
+  it(_ant.defineColumn.name, () => {
+    expect(_ant.defineColumn('Foo', 'foo')).toEqual({ dataIndex: 'foo', key: 'foo', title: 'Foo' })
+  })
+  it(_ant.defineColumnSortable.name, () => {
+    expect(_ant.defineColumnSortable('Foo', 'foo'))
+      .toEqual({ dataIndex: 'foo', key: 'foo', sorter: true, title: 'Foo' })
+  })
+  it(_ant.defineColumnSlotted.name, () => {
+    expect(_ant.defineColumnSlotted('Foo', 'foo', 'theSlot'))
+      .toEqual({ dataIndex: 'foo', key: 'foo', scopedSlots: { customRender: 'theSlot' }, title: 'Foo' })
+  })
+  it(_ant.defineChoices.name, () => {
+    let expectedResult = [
+      { key: 'none', label: '(None)' },
+      { key: 'one', label: 'one' },
+      { key: 'two', label: 'two' },
+    ]
+    const choices1 = ['one', 'two']
+    const choices2 = [{ key: 'one', label: 'one' }, { key: 'two', label: 'two' }]
+    const choices3 = [{ one: 'one' }, { two: 'two' }]
+    expect(_ant.defineChoices(choices1, true)).toEqual(expectedResult)
+    expect(_ant.defineChoices(choices2, true)).toEqual(expectedResult)
+    expect(_ant.defineChoices(choices3, true)).toEqual(expectedResult)
   })
 })
